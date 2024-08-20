d3067b0d00c988e2baf31724f360a7afc9634703
joaoevangelista <jesussave@gmail.com>
2024-08-20 15:52:11 -0300

    Alterado nome e valores da cc e pp

--- projeto-banco-digital/src/Main.java
+++ projeto-banco-digital/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		venilton.setNome("Eliah Azevedo");
 		
 		Conta cc = new ContaCorrente(venilton);
 		Conta poupanca = new ContaPoupanca(venilton);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
+		cc.depositar(200);
+		cc.transferir(150, poupanca);
 		
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
